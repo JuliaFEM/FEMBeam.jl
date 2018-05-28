8b706da95367f4287dfb781c66f844f10a739ac1
autodocs <autodocs>
2018-05-28 17:19:56 +0000

    build based on 3e6ccc3

---
+++ stable/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "Package contains beam formulations for JuliaFEM."
+},
+
+{
+    "location": "index.html#Theory-1",
+    "page": "Introduction",
+    "title": "Theory",
+    "category": "section",
+    "text": "Theory of beam formulations comes here."
+},
+
+{
+    "location": "index.html#Examples-1",
+    "page": "Introduction",
+    "title": "Examples",
+    "category": "section",
+    "text": "DocTestSetup = quote\n    using FEMBase\n    using FEMBeam\nendSimple calculation examples here."
+},
+
+]}
